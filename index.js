dc391522aa094bff7ec4037d90e7862e23c60859
Michael Scott <camrymps@gmail.com>
2017-07-10 15:15:44 -0700

    Moved vehicle parsing routine to its own method. 'getVehicle' method not longer uses 'getAllVehicles' method.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const _ = require('underscore')
 const querystring = require('querystring')
 const urlJoin = require('url-join')
 const async = require('async')
-require('promise')
+const promise = require('promise')
 const unirest = require('unirest')
 const requestHandler = require('unirest-request-handler')
 
@@ -16,135 +16,145 @@ const Trim = require('./lib/Trim')
 
 /**
  * Builds and executes a request.
- *
+ * 
  * @param {string} path The request's path
  * @param {Object} params The request's parameters
- *
+ * 
  * @return {Promise.<Object|Error>} The responses' body if fulfilled,
  * or an error if rejected
- *
+ * 
  */
-var doRequest = function (path, params) {
-  let endpoint = Helpers.absoluteURL(path + '?' + querystring.stringify(params))
-  return requestHandler.handle(unirest.get(endpoint))
+var doRequest = function(path, params) {
+    let endpoint = Helpers.absoluteURL(path + '?' + querystring.stringify(params));
+    return requestHandler.handle(unirest.get(endpoint));
 }
 
 /**
  * Client constructor.
  * @constructor
- *
+ * 
  */
-var ToyotaClient = function () {
-  let self = this
+var ToyotaClient = function() {
+    let self = this;
+
+    let parseVehicles = function(data) {
+        return new Promise(function(resolve, reject) {
+            async.map(data, function(vehicleData, callback) {
+                let vehicle = new Vehicle(vehicleData).getAll();
+
+                self.getGrades(vehicle.name, vehicle.year)
+                    .then(function(grades) {
+                        callback(null, _.extend(vehicle, grades));
+                    })
+                    .catch(callback);
+            }, function(err, vehicles) {
+                if (err) reject(err);
+                else resolve(vehicles);
+            });
+        });
+    }
 
     /**
      * Gets all vehicles.
-     *
+     * 
      * @return {Promise.<Object[]|Error>} An array of all vehicles if fulfilled,
      * or an error if rejected
-     *
+     * 
      */
-  this.getAllVehicles = function () {
-    return doRequest('ToyotaSite/rest/lscs/getDocument', {
-      templatePath: 'templatedata/TComVehiclesData/Series/data/CombinedSeries.xml'
-    })
-            .then(function (vehiclesData) {
-              return new Promise(function (resolve, reject) {
-                async.map(vehiclesData.Root.Series, function (vehicleData, callback) {
-                  let vehicle = new Vehicle(vehicleData).getAll()
-
-                  self.getGrades(vehicle.name, vehicle.year)
-                            .then(function (grades) {
-                              callback(null, _.extend(vehicle, grades))
-                            })
-                            .catch(callback)
-                }, function (err, vehicles) {
-                  if (err) reject(err)
-                  else resolve(vehicles)
-                })
-              })
+    this.getAllVehicles = function() {
+        return doRequest('ToyotaSite/rest/lscs/getDocument', {
+                templatePath: 'templatedata/TComVehiclesData/Series/data/CombinedSeries.xml'
             })
-            .catch(function (err) {
-              throw err
+            .then(function(vehiclesData) {
+                return parseVehicles(vehiclesData.Root.Series);
             })
-  }
+            .catch(function(err) {
+                throw err;
+            });
+    }
 
     /**
      * Gets a specific vehicle.
-     *
+     * 
      * @param {string} model The vehicle's name
      * @param {number} [year] The vehicle's year
-     *
+     * 
      * @return {Promise.<Object[]|Error>} An array of the specified vehicle's generations if
      * fulfilled, or an error if rejected
-     *
+     * 
      */
-  this.getVehicle = function (model, year) {
-    return self.getAllVehicles()
-            .then(function (vehiclesData) {
-              return _.where(vehiclesData, (year ? { name: model, year: parseInt(year) } : { name: model }))
+    this.getVehicle = function(model, year) {
+        return doRequest('ToyotaSite/rest/lscs/getDocument', {
+                templatePath: 'templatedata/TComVehiclesData/Series/data/CombinedSeries.xml'
             })
-            .catch(function (err) {
-              throw err
+            .then(function(vehiclesData) {
+                let vehicleGens = _.where(vehiclesData.Root.Series,
+                    (year ? { modelName: model, modelYear: parseInt(year) } : { modelName: model })
+                )
+                return parseVehicles(vehicleGens);
             })
-  }
+            .catch(function(err) {
+                throw err;
+            });
+    }
 
     /**
      * Gets a vehicle's grades and trims.
-     *
+     * 
      * @param {string} model The vehicle's name
      * @param {number} year The vehicle's year
-     *
+     * 
      * @return {Promise.<Object[]|Error>} An array of the specified vehicle's grades and trims if
      * fulfilled, or an error if rejected
-     *
+     * 
      */
-  this.getGrades = function (model, year) {
-    return doRequest('ToyotaSite/rest/lscs/getDocument', {
-      templatePath: urlJoin('templatedata/TComVehiclesData/VehicleTrim/data/', year, model.toLowerCase(), '.xml')
-    })
-            .then(function (data) {
-              if (data) {
-                let grades = _.map(data.Root.ModelGrades, function (gradeData) {
-                  let grade = new Grade(gradeData).getAll()
-                  let trims = _.map(gradeData.VehicleTrims, function (trimData) {
-                    return new Trim(trimData).getAll()
-                  })
-
-                  return _.extend(grade, { trims: trims })
-                })
-
-                return { grades: grades }
-              }
+    this.getGrades = function(model, year) {
+        return doRequest('ToyotaSite/rest/lscs/getDocument', {
+                templatePath: urlJoin('templatedata/TComVehiclesData/VehicleTrim/data/', year, model.toLowerCase(), '.xml')
             })
-            .catch(function (err) {
-              throw err
+            .then(function(data) {
+                if (data) {
+                    let grades = _.map(data.Root.ModelGrades, function(gradeData) {
+                        let grade = new Grade(gradeData).getAll();
+                        let trims = _.map(gradeData.VehicleTrims, function(trimData) {
+                            return new Trim(trimData).getAll();
+                        });
+
+                        return { grade: _.extend(grade, { trims: trims }) };
+                    });
+
+                    return { grades: grades };
+                }
             })
-  }
+            .catch(function(err) {
+                throw err;
+            })
+    }
 
     /**
      * Gets a list of dealers near a location.
-     *
+     * 
      * @param {number} zipCode A zip code
-     *
+     * 
      * @return {Promise.<Object[]|Error>} An array of dealers near the specified zip code if
      * fulfilled, or an error if rejected
-     *
+     * 
      */
-  this.getDealers = function (zipCode) {
-    return doRequest('ToyotaSite/rest/dealerLocator/locateDealers', {
-      brandId: 1,
-      zipCode: zipCode
-    })
-            .then(function (data) {
-              return _.map(data.dealers, function (dealerData) {
-                return new Dealer(dealerData).getAll()
-              })
+    this.getDealers = function(zipCode) {
+        return doRequest('ToyotaSite/rest/dealerLocator/locateDealers', {
+                brandId: 1,
+                zipCode: zipCode
             })
-            .catch(function (err) {
-              throw err
+            .then(function(data) {
+                return _.map(data.dealers, function(dealerData) {
+                    return new Dealer(dealerData).getAll();
+                });
             })
-  }
+            .catch(function(err) {
+                throw err;
+            });
+    }
+
 }
 
-module.exports = ToyotaClient
+module.exports = ToyotaClient;
